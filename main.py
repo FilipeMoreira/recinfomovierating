eaa806fc2b931552688c500d7191faa9590a1200
Filipe Moreira <filipe.moreira@visagio.com>
2018-07-10 00:48:53 -0300

    Creating matrix; Computing cosine distances

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy as np
 import io
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics.pairwise import pairwise_distances
 from math import sqrt
 
 def main():
@@ -9,6 +10,8 @@ def main():
 
     movie_names = []
     movie_count = 0
+    movie_ratings = []
+    users = []
 
     #reading movie title
     f = open('../movie_titles.txt', 'r', encoding = "ISO-8859-1")
@@ -22,17 +25,32 @@ def main():
     print(movie_count)
 
     #reading ratings
+    for i in range(movie_limit):
+        ratings = getRatingsForMovie(i)
+        movie_ratings.append(ratings)
+        #acquiring users
+        for user_id in ratings.keys():
+            if users.count(user_id) < 1:
+                users.append(user_id)
 
+    users.sort()
 
+    #fun begins
+    data_matrix = np.zeros((len(users), movie_limit))
+    for i in range(movie_limit):
+        for key in movie_ratings[i].keys():
+            data_matrix[key][i] = movie_ratings[i].get(key)
+
+    user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
+    movie_similarity = pairwise_distances(train_data_matrix.T, metric='cosine')
 
 def getRatingsForMovie(movie_id):
-    ratings = []
+    ratings = {}
     zeros = '0' * (7 - len(srt(movie_id)))
     f = open('../training_set/mv' + zeros + movie_id + '.txt', 'r', encoding = "ISO-8859-1")
     for line in f:
         values = line[:-1].split(',')
-        rating = { 'user_id': values[0], 'rate': values[1] }
-        ratings.append(rating)
+        ratings.[values[0]] = values[1]
     return ratings
 
 def rmse(prediction, ground_truth):
